5260f58d2b5368bc2edd5eea9eafb5833c76d4ee
Jemkin07 <gggonext05@gmail.com>
2024-03-04 17:44:17 +0545

    require packages

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const todoRoutes = express.Router()
+const app = express()
+
+app.use(cors());
+app.use()
+
